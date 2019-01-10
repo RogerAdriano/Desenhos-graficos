68513d3af67129aa6ac0f63a650322aa4812052e
Roger Adriano <rogeradriano1993@gmail.com>
2019-01-10 16:22:53 -0200

    Exemplo 2

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,18 +1,22 @@
 package aplicacao;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		
-		DrawPanel painel = new DrawPanel();
+		int choice = Integer.parseInt(JOptionPane.showInputDialog("Qual opção deseja:\n"
+				+ "1 - Retangulos\n2 - Circulos"));
+		
+		Shapes panel = new Shapes(choice);
 		
 		JFrame application = new JFrame();
 		
 		application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		application.add(painel);
-		application.setSize(250, 250);
+		application.add(panel);
+		application.setSize(300, 300);
 		application.setVisible(true);
 	}
 
